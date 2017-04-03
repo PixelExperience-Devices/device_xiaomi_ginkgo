41c53d286e9cfb11499038d3f11de5a3bffb3c26
Amit Shekhar <ashekhar@codeaurora.org>
2017-04-03 15:49:55 -0700

    ginkgo: mediacodec: Allow select and eventfd syscalls
    Add pselect6 and eventfd2 syscalls
    Includes following:
    mediacodec: allow sendto and recvfrom syscalls
    mediacodec: allow lseek syscall
    mediaextractor: allow pread64 and readlinkat syscalls
    mediacodec: allow getdents64, sysinfo and getcwd
    
    Vendor extended policy files are separated from the aosp
    and will now reside in /vendor
    
    Change-Id: If59d04b36d744efb74209ae36e76dd16cb18f606
    CRs-Fixed: 2027600

--- device.mk
+++ device.mk
@@ -335,6 +335,10 @@ PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
     librmnetctl
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     libsensorndkbridge \
