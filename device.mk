84d3fc6ad23e8e9e22c47fd51aba8ddfe71850ce
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    ginkgo: Build common USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- device.mk
+++ device.mk
@@ -255,6 +255,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
