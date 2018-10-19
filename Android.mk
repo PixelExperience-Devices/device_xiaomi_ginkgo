7138df86b1822f7b589d3f9c8baa439df03df4d1
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-10-19 17:11:10 +0200

    ginkgo: Make Android.mk search recursive
    Change-Id: I3a05344f949eae581f1f17cc33b1df475d3e6760
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- Android.mk
+++ Android.mk
@@ -6,8 +6,10 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter ginkgo willow,$(TARGET_DEVICE)),)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+ifeq ($(TARGET_DEVICE),ginkgo willow)
+
+subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
 include $(CLEAR_VARS)
 
