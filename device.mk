9db712ab7300142c9722245e9d89e479b1927404
kubersharma001 <kubersharma001@gmail.com>
2018-12-19 21:57:31 +0530

    ginkgo: Build telephony-ext
    Change-Id: I2dd7d94ba00af4d20aa2410b7c5ee54726a6ed59

--- device.mk
+++ device.mk
@@ -126,6 +126,13 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Vibrator
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/input/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
