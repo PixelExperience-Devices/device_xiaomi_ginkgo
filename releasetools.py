753fcd6add7e44a73bdea19212d6ac54420cbc0f
XiNGRZ <chenxingyu92@gmail.com>
2019-04-29 03:23:26 +0800

    g4w4: releasetools: Ship and update vbmeta image
    Change-Id: I8bb4da99945eceae3beab77489e6b9e2a47ab972
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -38,4 +38,5 @@ def AddImage(info, basename, dest):
 
 def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   return
