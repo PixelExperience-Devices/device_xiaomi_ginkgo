264e420d51a21062b7967c1e4a1a3d118e400417
Ethan Chen <intervigil@gmail.com>
2019-10-20 01:17:32 -0700

    ginkgo: Add AOSP RCS packages

--- device.mk
+++ device.mk
@@ -126,6 +126,12 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
