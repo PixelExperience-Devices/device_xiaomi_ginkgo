bbeefc53f00658c8c9830b6fb9f40af5b8ef7d53
Gaurav Singhal <gsinghal@codeaurora.org>
2019-12-26 17:55:32 +0530

    ginkgo: NFC: Enable build for 2.0 HIDL service.
    Change-Id: I71785d876cf4e154325a72e5c51d373ea61a73e6

--- device.mk
+++ device.mk
@@ -60,7 +60,7 @@ PRODUCT_PACKAGES += \
     com.nxp.nfc.nq \
     NQNfcNci \
     Tag \
-    vendor.nxp.hardware.nfc@1.2-service \
+    vendor.nxp.hardware.nfc@2.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
