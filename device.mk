b2cdd844f84678358d3db0c45a0312ec509aac5e
Fenglin Wu <fenglinw@codeaurora.org>
2020-03-05 19:31:14 +0800

    ginkgo: Exclude vibrator devices from input subsystem
    Haptics devices which registers as input FF devices are not supposed to
    be detected by input subsystem. Add excluded-input-devices.xml file for
    input subsystem to exclude them.
    
    Change-Id: I52dc6e502b29e2813299d073195a0991bb2cf692
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -69,4 +69,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Vibrator
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/input/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 PRODUCT_VENDOR_KERNEL_HEADERS := device/xiaomi/ginkgo-kernel/trinket/kernel-headers
