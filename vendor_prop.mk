2ebc41e929b2e608b67d9fcec767730353571914
Mrinal Ghosh <mg712702@gmail.com>
2020-05-07 10:14:05 +0530

    ginkgo: Disable bluetooth a2dp offload properly
    This reverts commit d77ada091785bea1808f2608e01b27cbeff57fa7.
    
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -97,9 +97,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.bluetooth.bluetooth_audio_hal.disabled=true \
     persist.vendor.bluetooth.modem_nv_support=true \
     persist.vendor.bt.aac_frm_ctl.enabled=true \
-    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptive \
-    persist.vendor.qcom.bluetooth.enable.splita2dp=false \
-    ro.bluetooth.a2dp_offload.supported=false \
+    persist.vendor.bt.enable.splita2dp=true \
+    persist.vendor.qcom.bluetooth.enable.splita2dp=true \
     vendor.bluetooth.soc=cherokee \
     vendor.qcom.bluetooth.soc=cherokee
 
