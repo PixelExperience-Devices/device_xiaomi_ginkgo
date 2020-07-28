568bd0828d1234e13feefc4a4b97ff0a6c4ed5e0
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-07-28 22:13:48 +0530

    ginkgo: Build camera hidl hals
    Change-Id: Ic87594e946c41413e8bfe5e7c3c37b67b0fed28a

--- device.mk
+++ device.mk
@@ -98,6 +98,17 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0 \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.device@3.5 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5 \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0.vendor
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.ginkgo
