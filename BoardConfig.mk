e3c5e98c1744fa9ebc8dc7535bdb7503388ce580
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-08-13 22:26:36 +0700

    g4w4: Define DT2W node

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,6 +161,9 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 TARGET_BOARD_PLATFORM := trinket
 TRINKET := trinket
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
