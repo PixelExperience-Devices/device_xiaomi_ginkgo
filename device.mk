f125fadbb5cc67863a5af91d6a7815209dcbd33b
Arian <arian.kulmer@web.de>
2020-10-26 23:22:55 +0000

    ginkgo: Build libdng_sdk.vendor

--- device.mk
+++ device.mk
@@ -107,7 +107,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    libdng_sdk.vendor
 
 # Fingerprint
 PRODUCT_PACKAGES += \
