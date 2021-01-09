3e27beebcb72bc1dcd7d4d8b0119d0b9ed677cd9
Jackeagle <jackeagle102@gmail.com>
2021-01-09 05:51:09 +0100

    ginkgo: Build ATRACE hidl hal
    Change-Id: I03d5713476f1d7de83774c62e36210f036299d95

--- device.mk
+++ device.mk
@@ -74,6 +74,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Audio
 PRODUCT_PACKAGES += \
     libaudio-resampler \
