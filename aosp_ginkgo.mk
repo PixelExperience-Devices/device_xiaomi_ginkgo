b796eb6c4b83a8acc7bfa478348abf244e1d6986
AndrielFR <andrielkogama2@gmail.com>
2021-02-03 14:27:21 -0300

    ginkgo: disable live wallpapers
    * A lot of lags.

--- aosp_ginkgo.mk
+++ aosp_ginkgo.mk
@@ -28,7 +28,7 @@ $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
