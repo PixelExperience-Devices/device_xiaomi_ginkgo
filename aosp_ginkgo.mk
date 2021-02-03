5c9ec6f91e630b981d1662fa144897a0b840e735
AndrielFR <andrielkogama2@gmail.com>
2021-02-03 17:48:42 -0300

    ginkgo: update redfin fingerprint to February

--- aosp_ginkgo.mk
+++ aosp_ginkgo.mk
@@ -41,8 +41,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ1A.210105.003 7005429 release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ1A.210205.004 7038034 release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
