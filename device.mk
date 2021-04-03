f09b86f61b19830f5775b7ecf16544aeed2b1be3
ghostrider-reborn <adithya.r02@outlook.com>
2021-04-03 21:45:33 +0530

    ginkgo: Add common config.fs AID/fs configuration file * from LA.UM.9.11.r1-02200-NICOBAR.0

--- device.mk
+++ device.mk
@@ -94,6 +94,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# AID/fs configs
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # Framework detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
