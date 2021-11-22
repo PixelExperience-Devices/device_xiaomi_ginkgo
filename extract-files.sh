e21402b56f77e9224b3bcb624f5e8c7799699ba1
Michael Bestas <mkbestas@lineageos.org>
2021-11-22 01:20:33 -0800

    ginkgo: Switch to patchelf from tools-lineage
    Change-Id: If1a7328804fb9bc39ccec70690f1090f2d0509b3

--- extract-files.sh
+++ extract-files.sh
@@ -60,7 +60,7 @@ fi
 function blob_fixup() {
     case "${1}" in
         vendor/bin/mlipayd@1.1 | vendor/lib64/libmlipay.so | vendor/lib64/libmlipay@1.1.so)
-            patchelf --remove-needed "vendor.xiaomi.hardware.mtdservice@1.0.so" "${2}"
+            "${PATCHELF}" --remove-needed "vendor.xiaomi.hardware.mtdservice@1.0.so" "${2}"
             ;;
         vendor/etc/camera/camera_config.xml)
             # Remove vtcamera for ginkgo
