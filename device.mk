9a0ca36716c390e5403323012ba7ef2e2a6e0ccb
DarkJoker360 <simoespo159@gmail.com>
2021-11-26 00:10:25 +0100

    ginkgo: Nuke atrace hal
    * Useless and logspamming.
    
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- device.mk
+++ device.mk
@@ -80,10 +80,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # A2DP
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
